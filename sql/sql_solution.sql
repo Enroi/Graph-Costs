076ec8bf51eef20c2452ca8c28e218c638138a6d
Vladimir Orlyanskiy <vorlyanskiy@ibs.ru>
2026-05-27 22:58:03 +0300

    небольшие правки скрипта sql_solution.sql

--- sql/sql_solution.sql
+++ sql/sql_solution.sql
@@ -4,7 +4,7 @@ DECLARE
     cost_row2 RECORD;
     formula VARCHAR;
     cost_sum NUMERIC(16, 2);
-    sum_of_costs NUMERIC(16, 2);
+--    sum_of_costs NUMERIC(16, 2);
     has_rows_for_divide boolean = TRUE;
     iteration_count INT = 0;
     step_starter_at TIMESTAMP;
@@ -107,5 +107,3 @@ BEGIN
     RAISE NOTICE '% %', 'Ошибки округления исправлены за: ', CLOCK_TIMESTAMP() - step_starter_at;
 
 END $$;
-
-SELECT statement_timestamp()
